c7ca40c70c0d12d50a736d9bec3b54302eb8b77e
Dlovanmaraan <123940259+Dlovanmaraan@users.noreply.github.com>
2023-03-25 02:13:33 +0300

    stateless widget

---
+++ lib/Dilovan/message.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class messageS extends StatelessWidget {
+  const messageS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
