ea032d3e21e0e653864f1772f42034130acfce5d
Dlovanmaraan <123940259+Dlovanmaraan@users.noreply.github.com>
2023-03-25 21:07:25 +0300

    message screen

--- lib/Dilovan/message.dart
+++ lib/Dilovan/message.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hw3/hw5data.dart';
+import 'package:flutter_hw3/messagedata.dart';
 
 class messageS extends StatelessWidget {
   const messageS({super.key});
@@ -6,7 +8,37 @@ class messageS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        
+      body: ListView.builder(
+        itemCount: DataList.length,
+        itemBuilder: (context, index) {
+        return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: DataList[index].img,
+              radius: 25,
+            ),
+            title: Row(
+              children:  [
+                Text(
+                  DataList[index].name,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 5),
+                Text(
+                  DataList[index].email,
+                  style: TextStyle(color: Colors.black54, fontSize: 14),
+                ),
+              ],
+            ),
+            subtitle: Text(
+              DataList[index].message,
+              style: TextStyle(color: Colors.black54, fontSize: 14),
+            ),
+          );
+      },),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.email_outlined),
+      ),  
     );
   }
 }
